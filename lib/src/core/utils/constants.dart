ec0c79288f2840f7096b887c6871253c207d29cd
Yazdan Mohammadi <yazdanmohammadi00@gmail.com>
2021-12-26 16:02:31 +0330

    add constant file

---
+++ lib/src/core/utils/constants.dart
@@ -0,0 +1,11 @@
+const String kMaterialAppTitle = 'Flutter clean Architecture';
+
+//  API
+const String kBaseUrl = 'https://newsapi.org/v2';
+
+const String kApiKey = '71a4b893b80a4a53879ddd800db1f4a0';
+
+//  Database
+const String kArticlesTableName = 'articles_table';
+
+const String kDatabaseName = 'app_database.db';
